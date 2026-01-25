8cd50f0a91b3a1e10241c42d8545562979bb9df3
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-01-25 00:17:24 +0000

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/newcode/',
   plugins: [react()],
-  root: 'src/react-app',
   build: {
-    outDir: '../../dist',
+    outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: 'index.html',
-    },
   },
 });
