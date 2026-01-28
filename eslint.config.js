dae687d59d7f9c0bc8f1f8b2f0cfed58dec9b404
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-01-28 05:48:33 +0000

    feat: add initial ESLint configuration file

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// eslint.config.js
+export default [
+  {
+    ignores: ['**/dist/**', '**/node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      // Add ESLint rules here
+    },
+  },
+];
