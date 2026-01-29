85c956bdf24f76406f9e3ce7180a0038db3df46b
Adriano Hermida Maia <42121091+adrianohermida@users.noreply.github.com>
2026-01-29 01:24:38 +0000

    fix: move rollupOptions inside build configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
 });
